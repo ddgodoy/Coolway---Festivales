35da4fec855b2a0d4e7cfd9df3543ea3eeb6c329
mauro <mauro@icox.com.ar>
2015-06-05 16:15:27 -0300

    update ranking

--- src/CoolwayFestivales/BackendBundle/Controller/ApiController.php
+++ src/CoolwayFestivales/BackendBundle/Controller/ApiController.php
@@ -263,7 +263,7 @@ class ApiController extends Controller {
                     'name' => $r['user'],
                     'favorite' => isset($favorites[$r['user_id']]) || $r['user_id'] == $user->getId() ? 1 : 0,
                 );
-                if($i == 100)
+                if($i == 200)
                     break;
                 $i++;
             }
