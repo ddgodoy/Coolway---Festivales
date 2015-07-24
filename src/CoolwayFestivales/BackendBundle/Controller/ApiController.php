a5397eef4d2e4e87260deafcd2ee03cddd7fa104
Marcos Piñero <pinero.marcos@gmail.com>
2015-07-24 12:54:27 -0300

    3km

--- src/CoolwayFestivales/BackendBundle/Controller/ApiController.php
+++ src/CoolwayFestivales/BackendBundle/Controller/ApiController.php
@@ -930,7 +930,7 @@ class ApiController extends Controller {
             $dist = rad2deg($dist);
             $miles = $dist * 60 * 1.1515;
             $km = $miles * 1.609344;
-            if($km <= 50)
+            if($km <= 3)
                 return true;
         }
         return false;
