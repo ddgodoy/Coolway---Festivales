48effeb18b256061c166b89d030fac9c1a2707bb
Nestor Sanchez <nsanchez@smartbeemo.com>
2016-04-14 20:04:34 -0430

    Modified UserController & Adding AuthentincationController

---
+++ src/CoolwayFestivales/ApiBundle/Controller/AuthenticationController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace CoolwayFestivales\ApiBundle\Controller;
+
+use CoolwayFestivales\SafetyBundle\Entity\Role;
+use CoolwayFestivales\SafetyBundle\Entity\User;
+use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AuthenticationController extends FOSRestController implements ClassResourceInterface
+{
+
+
+    /**
+     * @param Request $request
+     * @return array
+     *
+     * @ApiDoc(
+     *  section="Authentication",
+     *  description="Authentication",
+     *  requirements={
+     *      {"name"="email", "dataType"="string", "requirement"="/^[A-Za-z0-9 _.-]+$/", "description"="Email Address"},
+     *      {"name"="password", "dataType"="string", "requirement"="/^[A-Za-z0-9 _.-]+$/", "description"="Password"},
+     *   },
+     *  statusCodes={
+     *         200="Returned when successful"
+     *  },
+     *  tags={
+     *   "stable" = "#4A7023",
+     *   "v1" = "#ff0000"
+     *  }
+     * )
+     */
+    public function postAction(Request $request)
+    {
+        $response = new Response();
+        $email = $request->get('email');
+        $password = $request->get('password');
+        $social = $request->get('social', false);
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('SafetyBundle:User')
+            ->findOneBy(array('email' => $email));
+
+        if ($social && $user) {
+            $user->setAccessToken(md5($user->getId() . '-' . date('Y-m-d-H-i-s')));
+            $em->persist($user);
+            $em->flush();
+
+            $response->setContent(json_encode(array(
+                'access_token' => $user->getAccessToken(),
+                'name' => $user->getName(),
+                'email' => $user->getEmail()
+            )));
+
+        }else if($user) {
+            $encoder = $this->get('security.encoder_factory')->getEncoder($user);
+            $passwordEncoded = $encoder->encodePassword($password, $user->getSalt());
+
+            if ($passwordEncoded === $user->getPassword()) {
+                $user->setAccessToken(md5($user->getId() . '-' . date('Y-m-d-H-i-s')));
+                $em->persist($user);
+                $em->flush();
+
+                $response->setContent(json_encode(array(
+                    'access_token' => $user->getAccessToken(),
+                    'name' => $user->getName(),
+                    'email' => $user->getEmail()
+                )));
+            }else
+                throw new HttpException(400, "Datos invalidos");
+        }else
+            throw new HttpException(400, "Datos invalidos");
+
+        return $response;
+    }
+
+
+
+
+}
