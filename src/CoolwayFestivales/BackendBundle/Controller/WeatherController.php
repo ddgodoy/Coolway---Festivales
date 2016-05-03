bb75ccac0a336e02aa5f86fd01da55ae160215c9
Nestor Sanchez <sanchez91nestor@icox.com>
2016-05-03 15:31:04 -0400

    Adding Weather Controller

---
+++ src/CoolwayFestivales/BackendBundle/Controller/WeatherController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace CoolwayFestivales\BackendBundle\Controller;
+
+use CoolwayFestivales\BackendBundle\Entity\NotificationStats;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Acl\Permission\MaskBuilder;
+use CoolwayFestivales\BackendBundle\Form\NotificationType;
+use CoolwayFestivales\SafetyBundle\Entity\User;
+
+/**
+ * Notification controller.
+ *
+ * @Route("/admin/weather")
+ */
+class WeatherController extends Controller
+{
+
+
+    /**
+     * @Route("/update", name="weather_update")
+     * @Method("GET")
+     */
+    public function notificationSendAllAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $festivals = $em->getRepository('BackendBundle:Feast')->findAll();
+
+        foreach ($festivals as $value)
+        {
+            if (!empty($value->getLatitude()) && !empty($value->getLongitude()))
+            {
+                $em->getRepository('BackendBundle:Weather')->setForecastInfo
+                (
+                    $value,
+                    $value->getLatitude(),
+                    $value->getLongitude()
+                );
+            }
+        }
+
+
+        return new Response('true');
+    }
+
+
+
+} // end class
