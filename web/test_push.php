5e80d9c6697c167c6d1fbdeb5682e6be0585a9b7
mauro <mauro@icox.com.ar>
2016-06-16 11:14:14 -0300

    add cert prod cabo de plata

--- web/test_push.php
+++ web/test_push.php
@@ -15,7 +15,7 @@ stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
  
 // Abrimos conexión con APNS
 $fp = stream_socket_client(
-	'ssl://gateway.push.apple.com:2195', $err,
+	'ssl://gateway.sandbox.push.apple.com:2195', $err,
 	$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
  
 if (!$fp) {
